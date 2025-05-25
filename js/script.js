fdec0ff26fc418415d5dc32187fbf106aa5967ba
manuelalacerda <manuelalacerda88@gmail.com>
2025-05-25 02:16:38 -0300

    Função atacar() com efeitos no DOM

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ let gorillaLife = 100;
 let humans = Array(100).fill(true);
 let defending = false;
 let attackInterval;
+
+
+function gorillaAttack() {
+  showAttackEffect();
+  const toKill = Math.floor(Math.random() * 6) + 3;
+  let killed = 0;
+  for (let i = 0; i < humans.length && killed < toKill; i++) {
+    if (humans[i]) {
+      humans[i] = false;
+      killed++;
+    }
+  }
+  log(`🦍 O gorila atacou e derrotou ${killed} humanos.`);
+  checkEndGame();
+  updateStatus();
+}
